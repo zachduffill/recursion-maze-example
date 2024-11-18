e8682866eb900578e63aacc5e3683f6a0d874f99
zachduffill <zachdach321@googlemail.com>
2024-11-18 17:51:27 +0000

    Created html grid, styled with css, started maze.js

---
+++ maze.js
@@ -0,0 +1,12 @@
+const wall = "#";
+const empty = "-";
+const start = "S";
+const end = "E";
+
+function generate(size){
+    let arr = new Array();
+
+    for (let i = 1; i < size-1; ++i){
+
+    } // you might also need to use recursive funcs for generating valid mazes
+}
