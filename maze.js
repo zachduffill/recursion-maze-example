f0baeefe82d5c1a5133b2086e31d52efc6ec2bee
zachduffill <zachdach321@googlemail.com>
2024-11-19 13:52:49 +0000

    Added setGridSize() for automatic grid creation and sizing

--- maze.js
+++ maze.js
@@ -3,6 +3,10 @@ const empty = "-";
 const start = "S";
 const end = "E";
 
+$(document).ready(function () {
+    setGridSize(7);
+});
+
 function generate(size){
     let arr = new Array();
 
@@ -10,3 +14,12 @@ function generate(size){
 
     } // you might also need to use recursive funcs for generating valid mazes
 }
+
+function setGridSize(size){
+    let grid = document.getElementById("maze-grid");
+    grid.style.gridTemplateRows = "repeat("+size+", 50px)"
+    grid.style.gridTemplateColumns = "repeat("+size+", 50px)"
+
+    grid.innerHTML = '<div class="maze-node"></div>'.repeat(size**2);
+}
+
